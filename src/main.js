ca2a2c31c41f645dc69e032b6ab47535eda04383
shin <38515249+shin-elab@users.noreply.github.com>
2020-07-29 10:07:20 +0900

    feat: import some packages

--- src/main.js
+++ src/main.js
@@ -2,8 +2,15 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
+import axios from "axios";
+import VueAxios from "vue-axios";
+
+import "aframe";
+import "@/assets/css/tailwind.css";
 
 Vue.config.productionTip = false;
+
+Vue.use(VueAxios, axios);
 
 new Vue({
   router,
