bc364acf428c82858d07fe8f70ea44487b998f67
shin <38515249+shin-elab@users.noreply.github.com>
2020-07-29 22:42:46 +0900

    test: add atoms/Logo image src test

--- src/components/atoms/Logo/index.spec.js
+++ src/components/atoms/Logo/index.spec.js
@@ -11,4 +11,38 @@ describe("atoms/Logo", () => {
     });
     expect(wrapper.attributes("width")).toEqual(width.toString());
   });
+
+  it("logo image is changed by props.isHorizontal", async () => {
+    // `require()` does not work in unit test.
+    const dummy_wrapper = shallowMount(Logo);
+
+    dummy_wrapper.setProps({ isHorizontal: true });
+    await dummy_wrapper.vm.$nextTick();
+    expect(dummy_wrapper.attributes("src")).toEqual("");
+
+    dummy_wrapper.setProps({ isHorizontal: false });
+    await dummy_wrapper.vm.$nextTick();
+    expect(dummy_wrapper.attributes("src")).toEqual("");
+
+    // re-write `Logo.computed.imageSrc` to return `require()` as string
+    Logo.computed.imageSrc = function() {
+      if (this.isHorizontal) {
+        return 'require("@/assets/images/logo-h.png")';
+      } else {
+        return 'require("@/assets/images/logo-v.png")';
+      }
+    };
+    const wrapper = shallowMount(Logo);
+    wrapper.setProps({ isHorizontal: true });
+    await wrapper.vm.$nextTick();
+    expect(wrapper.attributes("src")).toEqual(
+      'require("@/assets/images/logo-h.png")'
+    );
+
+    wrapper.setProps({ isHorizontal: false });
+    await wrapper.vm.$nextTick();
+    expect(wrapper.attributes("src")).toEqual(
+      'require("@/assets/images/logo-v.png")'
+    );
+  });
 });
