304840c0d123d7f0e06ad560d655251d81b93a38
shin <38515249+shin-elab@users.noreply.github.com>
2020-07-29 15:53:19 +0900

    feat: add atoms/Logo component

---
+++ src/components/atoms/Logo/index.vue
@@ -0,0 +1,12 @@
+<template>
+  <img :width="width" alt="Web360Square" src="@/assets/images/logo-white.png" />
+</template>
+
+<script>
+export default {
+  name: "Logo",
+  props: {
+    width: Number,
+  },
+};
+</script>
