8909ff69c79d80a4555ec934eec251f80e8c0a1f
shin <38515249+shin-elab@users.noreply.github.com>
2020-07-29 22:42:06 +0900

    test: update atoms/Logo width test

--- src/components/atoms/Logo/index.spec.js
+++ src/components/atoms/Logo/index.spec.js
@@ -3,7 +3,9 @@ import Logo from "./";
 
 describe("atoms/Logo", () => {
   it("logo width is equal to props.width", () => {
-    const width = 240;
+    const max = 9900;
+    const min = 100;
+    const width = Math.floor(Math.random() * (max + 1 - min)) + min;
     const wrapper = shallowMount(Logo, {
       propsData: { width: width },
     });
