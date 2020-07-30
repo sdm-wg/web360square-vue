e008fdb6cfcf3499a12f264b9d44ee1ff5b87205
shin <38515249+shin-elab@users.noreply.github.com>
2020-07-30 22:24:22 +0900

    feat: add atoms/CarouselIndexButton component

---
+++ src/components/atoms/CarouselIndexButton/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <button
+    class="flex-1 w-4 h-2 mt-4 mx-2 mb-0 rounded-full overflow-hidden transition-colors duration-200 ease-out hover:bg-orange-600"
+    :class="{
+      'bg-orange-400': isActive,
+      'bg-orange-200': !isActive,
+    }"
+  ></button>
+</template>
+
+<script>
+export default {
+  name: "CarouselIndexButton",
+  props: {
+    isActive: Boolean,
+  },
+};
+</script>
