b4b4cc1cee8f6959c6b0609da0436d2c00db2144
shin <38515249+shin-elab@users.noreply.github.com>
2020-07-31 14:34:56 +0900

    feat: update pages/Home component

--- src/components/pages/Home/index.vue
+++ src/components/pages/Home/index.vue
@@ -1,9 +1,41 @@
 <template>
-  <div></div>
+  <HomeView :isFirstView="isFirstView" :logos="logos" :events="events" />
 </template>
 
 <script>
+import HomeView from "@/components/templates/HomeView";
+
 export default {
   name: "Home",
+  data: () => {
+    return {
+      isFirstView: true,
+      logos: {
+        firstView: {
+          width: 280,
+          isHorizontal: false,
+        },
+        mainView: {
+          width: 180,
+          isHorizontal: true,
+        },
+      },
+      events: [],
+    };
+  },
+  created: function() {
+    // Dummy data-fetch process
+    const toggleViewDelay = 1000;
+    setTimeout(() => {
+      this.isFirstView = false;
+
+      // For caroucel debug
+      const eventN = 5;
+      this.events = new Array(eventN);
+    }, toggleViewDelay);
+  },
+  components: {
+    HomeView,
+  },
 };
 </script>
