46dffeb947e2b724a1c77f5034a4ebeabbfe93d4
shin <38515249+shin-elab@users.noreply.github.com>
2020-08-01 13:03:41 +0900

    feat: add card logo to organisms/CarouselCard

--- src/components/organisms/CarouselCard/index.vue
+++ src/components/organisms/CarouselCard/index.vue
@@ -4,16 +4,29 @@
     class="w-full h-full flex bg-white text-black rounded-lg overflow-hidden leading-none"
   >
     <router-link to="/" class="w-full h-full flex items-center z-10">
+      <!-- Carousel Card Logo -->
+      <div
+        class="w-1/3 h-full bg-orange-500 hover:bg-red-500 flex justify-center items-center"
+      >
+        <div class="w-1/3">
+          <PlaySVG class="w-full" />
+        </div>
+      </div>
     </router-link>
   </div>
 </template>
 
 <script>
+import PlaySVG from "@/components/atoms/PlaySVG";
+
 export default {
   name: "CarouselCard",
   props: {
     isActive: Boolean,
     event: Object,
+  },
+  components: {
+    PlaySVG,
   },
 };
 </script>
