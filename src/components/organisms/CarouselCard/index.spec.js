4b2a4b37ea7127d1c802a355f87f9a773066e076
shin <38515249+shin-elab@users.noreply.github.com>
2020-08-01 14:58:55 +0900

    test: add CarouselCardEventName test to organisms/CarouselCard

--- src/components/organisms/CarouselCard/index.spec.js
+++ src/components/organisms/CarouselCard/index.spec.js
@@ -2,6 +2,7 @@ import { shallowMount } from "@vue/test-utils";
 import { toBeVisible } from "@testing-library/jest-dom/matchers";
 import CarouselCard from ".";
 import PlaySVG from "@/components/atoms/PlaySVG";
+import CarouselCardEventName from "@/components/molecules/CarouselCardEventName";
 
 expect.extend({ toBeVisible });
 
@@ -36,5 +37,18 @@ describe("organisms/CarouselCard", () => {
       stubs: ["router-link"],
     });
     expect(wrapper.findComponent(PlaySVG).exists()).toBe(true);
+  });
+
+  it("has a CarouselCardEventName component", () => {
+    const event = {
+      name: "",
+      date: "",
+      place: { name: "", address: "" },
+    };
+    const wrapper = shallowMount(CarouselCard, {
+      propsData: { event: event },
+      stubs: ["router-link"],
+    });
+    expect(wrapper.findComponent(CarouselCardEventName).exists()).toBe(true);
   });
 });
