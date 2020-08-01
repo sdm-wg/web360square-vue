c5348d41217d10bfa155c1f6ff81c85b24e23e7f
shin <38515249+shin-elab@users.noreply.github.com>
2020-08-01 15:25:22 +0900

    test: add CarouselCardEventPlace test to organisms/CarouselCard

--- src/components/organisms/CarouselCard/index.spec.js
+++ src/components/organisms/CarouselCard/index.spec.js
@@ -4,6 +4,7 @@ import CarouselCard from ".";
 import PlaySVG from "@/components/atoms/PlaySVG";
 import CarouselCardEventName from "@/components/molecules/CarouselCardEventName";
 import CarouselCardEventDate from "@/components/molecules/CarouselCardEventDate";
+import CarouselCardEventPlace from "@/components/molecules/CarouselCardEventPlace";
 
 expect.extend({ toBeVisible });
 
@@ -64,5 +65,18 @@ describe("organisms/CarouselCard", () => {
       stubs: ["router-link"],
     });
     expect(wrapper.findComponent(CarouselCardEventDate).exists()).toBe(true);
+  });
+
+  it("has a CarouselCardEventPlace component", () => {
+    const event = {
+      name: "",
+      date: "",
+      place: { name: "", address: "" },
+    };
+    const wrapper = shallowMount(CarouselCard, {
+      propsData: { event: event },
+      stubs: ["router-link"],
+    });
+    expect(wrapper.findComponent(CarouselCardEventPlace).exists()).toBe(true);
   });
 });
