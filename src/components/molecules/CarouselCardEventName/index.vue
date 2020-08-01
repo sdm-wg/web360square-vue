05ea439c46082edc7b5992876e96a628d4bca593
shin <38515249+shin-elab@users.noreply.github.com>
2020-08-01 14:30:01 +0900

    feat: add molecules/CarouselCardEventName component

---
+++ src/components/molecules/CarouselCardEventName/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="w-full px-1 flex items-center">
+    <span class="w-full font-bold text-2xl md:text-3xl lg:text-4xl text-center">
+      {{ name }}
+    </span>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "CarouselCardEventName",
+  props: {
+    name: String,
+  },
+};
+</script>
