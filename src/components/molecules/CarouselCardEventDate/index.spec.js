61da5393732ac756a473a62f1ed0ede55a6318fd
shin <38515249+shin-elab@users.noreply.github.com>
2020-08-01 15:03:51 +0900

    test: add molecules/CarouselCardEventDate test

---
+++ src/components/molecules/CarouselCardEventDate/index.spec.js
@@ -0,0 +1,15 @@
+import { shallowMount } from "@vue/test-utils";
+import CarouselCardEventDate from ".";
+
+describe("molecules/CarouselCardEventDate", () => {
+  it("checks props.name", () => {
+    // Generate a random string
+    const date = Math.random()
+      .toString(36)
+      .substring(2, 15);
+    const wrapper = shallowMount(CarouselCardEventDate, {
+      propsData: { date: date },
+    });
+    expect(wrapper.props("date")).toBe(date);
+  });
+});
