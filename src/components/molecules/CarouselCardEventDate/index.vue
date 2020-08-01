a8cceed0e5d241ac5b01490e42152f8a65a59801
shin <38515249+shin-elab@users.noreply.github.com>
2020-08-01 15:01:25 +0900

    feat: add molecules/CarouselCardEventDate component

---
+++ src/components/molecules/CarouselCardEventDate/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="w-full px-1 flex items-center">
+    <span class="w-full font-bold text-xl md:text-2xl lg:text-3xl">
+      {{ date }}
+    </span>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "CarouselCardEventDate",
+  props: {
+    date: String,
+  },
+};
+</script>
