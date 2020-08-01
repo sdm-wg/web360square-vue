f47f03287358da4426c5803c8e93bb7c495f8941
shin <38515249+shin-elab@users.noreply.github.com>
2020-08-01 17:25:47 +0900

    refactor: do not use `@fullhuman/postcss-purgecss`

--- postcss.config.js
+++ postcss.config.js
@@ -1,27 +1,5 @@
-const autoprefixer = require("autoprefixer");
 const tailwindcss = require("tailwindcss");
-const purgecss = require("@fullhuman/postcss-purgecss")({
-  content: ["./public/**/*.html", "./src/**/*.vue"],
-  defaultExtractor: (content) => {
-    const contentWithoutStyleBlocks = content.replace(
-      /<style[^]+?<\/style>/gi,
-      ""
-    );
-    return (
-      contentWithoutStyleBlocks.match(/[A-Za-z0-9-_/:]*[A-Za-z0-9-_/]+/g) || []
-    );
-  },
-  whitelistPatterns: [
-    /-(leave|enter|appear)(|-(to|from|active))$/,
-    /^(?!cursor-move).+-move$/,
-    /^router-link(|-exact)-active$/,
-  ],
-});
 
 module.exports = {
-  plugins: [
-    autoprefixer,
-    tailwindcss,
-    ...(process.env.NODE_ENV === "production" ? [purgecss] : []),
-  ],
+  plugins: [tailwindcss],
 };
---
+++ tailwind.config.js
@@ -0,0 +1,26 @@
+module.exports = {
+  purge: {
+    mode: "all",
+    content: ["./public/**/*.html", "./src/**/*.vue"],
+    options: {
+      defaultExtractor: (content) => {
+        const contentWithoutStyleBlocks = content.replace(
+          /<style[^]+?<\/style>/gi,
+          ""
+        );
+        return (
+          contentWithoutStyleBlocks.match(/[A-Za-z0-9-_/:]*[A-Za-z0-9-_/]+/g) ||
+          []
+        );
+      },
+      whitelistPatterns: [
+        /-(leave|enter|appear)(|-(to|from|active))$/,
+        /^(?!cursor-move).+-move$/,
+        /^router-link(|-exact)-active$/,
+      ],
+    },
+  },
+  theme: {},
+  variants: {},
+  plugins: [],
+};
