b6a30de16362409ba98933425540968a64ae879f
shin <38515249+shin-elab@users.noreply.github.com>
2020-08-01 15:20:46 +0900

    feat: add molecules/CarouselCardEventPlace component

---
+++ src/components/molecules/CarouselCardEventPlace/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="w-full px-1 flex flex-col justify-center">
+    <span class="w-full font-bold text-xl md:text-2xl lg:text-3xl">
+      {{ place.name }}
+    </span>
+    <span class="w-full text-md md:text-lg lg:text-xl">
+      {{ place.address }}
+    </span>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "CarouselCardEventPlace",
+  props: {
+    place: Object,
+  },
+};
+</script>
