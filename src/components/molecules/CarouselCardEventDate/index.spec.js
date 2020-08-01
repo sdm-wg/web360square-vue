53cea7789970eb3d7b1002092e875cf2dbd74d49
shin <38515249+shin-elab@users.noreply.github.com>
2020-08-01 16:29:13 +0900

    test: add formattedDate test to molecules/CarouselCardEventDate

--- src/components/molecules/CarouselCardEventDate/index.spec.js
+++ src/components/molecules/CarouselCardEventDate/index.spec.js
@@ -1,4 +1,5 @@
 import { shallowMount } from "@vue/test-utils";
+import moment from "moment";
 import CarouselCardEventDate from ".";
 
 describe("molecules/CarouselCardEventDate", () => {
@@ -11,5 +12,22 @@ describe("molecules/CarouselCardEventDate", () => {
       propsData: { date: date },
     });
     expect(wrapper.props("date")).toBe(date);
+  });
+
+  it("checks `formattedDate` (computed function)", async () => {
+    const wrapper = shallowMount(CarouselCardEventDate);
+    const date1 = "2020-08-01T16:00:00.000+09:00";
+    wrapper.setProps({ date: date1 });
+    await wrapper.vm.$nextTick();
+    expect(wrapper.find("span").text()).toBe(
+      moment(Date.parse(date1)).format("YYYY-MM-DD HH:mm:SS")
+    );
+
+    const date2 = "1970-01-01T00:00:00.000";
+    wrapper.setProps({ date: date2 });
+    await wrapper.vm.$nextTick();
+    expect(wrapper.find("span").text()).toBe(
+      moment(Date.parse(date2)).format("YYYY-MM-DD HH:mm:SS")
+    );
   });
 });
