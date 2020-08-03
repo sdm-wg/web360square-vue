667cdba0b862145033e760dc47155d0dd1a0571b
shin <38515249+shin-elab@users.noreply.github.com>
2020-08-03 10:28:22 +0900

    test: add (empty) test to pages/Event

---
+++ src/components/pages/Event/index.spec.js
@@ -0,0 +1,9 @@
+import { shallowMount } from "@vue/test-utils";
+import Event from ".";
+
+describe("pages/Event", () => {
+  // Dummy Test
+  it("", () => {
+    const wrapper = shallowMount(Event);
+  });
+});
