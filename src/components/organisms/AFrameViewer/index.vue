e17cd14ae7f5275b11a2de7e8fed01d22400ac7c
shin <38515249+shin-elab@users.noreply.github.com>
2020-08-03 22:11:35 +0900

    feat: add organisms/AFrameViewer component (WIP)

---
+++ src/components/organisms/AFrameViewer/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <a-scene
+    vr-mode-ui="enabled: false"
+    loading-screen="dotsColor: white; backgroundColor: black"
+  >
+  </a-scene>
+</template>
+
+<script>
+export default {
+  name: "AFrameViewer",
+  props: {
+    viewerData: Object,
+  },
+};
+</script>
