02c00d91f04c9d86a12b6fb1a91594cd49c47d0c
shin <38515249+shin-elab@users.noreply.github.com>
2020-08-03 22:16:18 +0900

    feat: add organisms/SideController component

---
+++ src/components/organisms/SideController/index.vue
@@ -0,0 +1,24 @@
+<template>
+  <nav
+    class="h-full flex justify-center items-center bg-black bg-opacity-25 text-gray-100 z-9999"
+  >
+    <ul>
+      <li class="hover:bg-gray-700">
+        <a @click.prevent="" href="#" class="w-16 h-16 p-4 flex">
+          <PlaySVG class="w-full text-white" :isPlay="true" />
+        </a>
+      </li>
+    </ul>
+  </nav>
+</template>
+
+<script>
+import PlaySVG from "@/components/atoms/PlaySVG";
+
+export default {
+  name: "sideController",
+  components: {
+    PlaySVG,
+  },
+};
+</script>
