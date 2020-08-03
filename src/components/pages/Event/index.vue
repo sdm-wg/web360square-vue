9058afadc82889e2d2ba0a5562f0d447688f2262
shin <38515249+shin-elab@users.noreply.github.com>
2020-08-03 13:53:23 +0900

    feat: load viewer data

--- src/components/pages/Event/index.vue
+++ src/components/pages/Event/index.vue
@@ -3,7 +3,35 @@
 </template>
 
 <script>
+import {
+  sparqlAxios,
+  sparqlEndpointUrl,
+  viewerQuery,
+  parseViewer,
+} from "@/utils/sparql.js";
+
 export default {
   name: "Event",
+  data: () => {
+    return {
+      viewerData: parseViewer([]),
+    };
+  },
+  created: function() {
+    const eventId = this.$route.query.id;
+
+    sparqlAxios(
+      this.axios,
+      `${sparqlEndpointUrl}?query=${encodeURIComponent(viewerQuery(eventId))}`,
+      (response) => {
+        const dataArray = response.data.results.bindings;
+        this.viewerData = parseViewer(dataArray);
+      },
+      (error) => {
+        console.error(error);
+        this.viewerData = parseViewer([]);
+      }
+    );
+  },
 };
 </script>
