0f958b85e49c40d6dde31d4afb46892f9e9f67ba
shin <38515249+shin-elab@users.noreply.github.com>
2020-08-04 01:39:43 +0900

    test: add atoms/AFrameAudioVisualizer test

---
+++ src/components/atoms/AFrameAudioVisualizer/index.spec.js
@@ -0,0 +1,12 @@
+import { shallowMount } from "@vue/test-utils";
+import AFrameAudioVisualizer from ".";
+
+describe("atoms/AFrameAudioVisualizer", () => {
+  it("has an `a-entity`", () => {
+    const wrapper = shallowMount(AFrameAudioVisualizer, {
+      stubs: ["a-entity"],
+    });
+    // Hack
+    expect(wrapper.find("a-entity-stub").exists()).toBe(true);
+  });
+});
