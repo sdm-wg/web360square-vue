8e8850a0674b8e81baea25a4843ffa5ba4b084df
shin <38515249+shin-elab@users.noreply.github.com>
2020-08-04 01:38:38 +0900

    feat: add some components to organisms/AFrameViewer

--- src/components/organisms/AFrameViewer/index.vue
+++ src/components/organisms/AFrameViewer/index.vue
@@ -3,14 +3,33 @@
     vr-mode-ui="enabled: false"
     loading-screen="dotsColor: white; backgroundColor: black"
   >
+    <AFrameAssets :playlistFile="viewerData.playlistFile" />
+    <AFrameCamera />
+    <AFrameVideoSphere />
+    <AFrameAudioVisualizer
+      v-for="(position, audioVisualizerIndex) in viewerData.positions"
+      :key="audioVisualizerIndex"
+      :position="position"
+    />
   </a-scene>
 </template>
 
 <script>
+import AFrameAudioVisualizer from "@/components/atoms/AFrameAudioVisualizer";
+import AFrameVideoSphere from "@/components/atoms/AFrameVideoSphere";
+import AFrameAssets from "@/components/molecules/AFrameAssets";
+import AFrameCamera from "@/components/molecules/AFrameCamera";
+
 export default {
   name: "AFrameViewer",
   props: {
     viewerData: Object,
+  },
+  components: {
+    AFrameAudioVisualizer,
+    AFrameVideoSphere,
+    AFrameAssets,
+    AFrameCamera,
   },
 };
 </script>
