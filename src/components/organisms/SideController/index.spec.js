67b15e0c4a971ddc4fa89cce003f5d3ccbcd4e79
shin <38515249+shin-elab@users.noreply.github.com>
2020-08-03 22:45:49 +0900

    test: add Logo test to organisms/SideController

--- src/components/organisms/SideController/index.spec.js
+++ src/components/organisms/SideController/index.spec.js
@@ -1,10 +1,20 @@
 import { shallowMount } from "@vue/test-utils";
 import SideController from ".";
 import PlaySVG from "@/components/atoms/PlaySVG";
+import Logo from "@/components/atoms/Logo";
 
 describe("organisms/SideController", () => {
   it("has a PlaySVG component", () => {
-    const wrapper = shallowMount(SideController);
+    const wrapper = shallowMount(SideController, {
+      stubs: ["router-link"],
+    });
     expect(wrapper.findComponent(PlaySVG).exists()).toBe(true);
+  });
+
+  it("has a Logo component", () => {
+    const wrapper = shallowMount(SideController, {
+      stubs: ["router-link"],
+    });
+    expect(wrapper.findComponent(Logo).exists()).toBe(true);
   });
 });
