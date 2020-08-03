9be52d27f53ba1b3851ebfedbb96f70f3c5599a9
shin <38515249+shin-elab@users.noreply.github.com>
2020-08-03 13:49:03 +0900

    feat: add viewer SPARQL script

--- src/utils/sparql.js
+++ src/utils/sparql.js
@@ -27,6 +27,36 @@ select distinct ?event ?eventName ?eventDate ?eventPlaceName ?eventPlaceAddress 
 }
 `;
 
+export const viewerQuery = (eventId) => {
+  return `\
+    PREFIX schema: <http://schema.org/>
+    PREFIX sdm: <http://sdm.hongo.wide.ad.jp/resource/>
+    PREFIX sdmo: <http://sdm.hongo.wide.ad.jp/sdmo/>
+
+    select distinct ?playerClass ?contentUrl ?eventTime ?startAt ?endAt ?x ?y ?z where {
+        ?player
+            a ?playerClass ;
+            schema:name "Web360Square" ;
+            sdmo:plays sdm:${eventId} ;
+            sdmo:inputFrom ?compositeMedia.
+        ?compositeMedia
+            schema:contentUrl ?contentUrl ;
+            sdmo:includes ?mediaEvent .
+        ?mediaEvent
+            sdmo:refers ?media ;
+            sdmo:eventTime ?eventTime .
+        ?media
+            sdmo:inputFrom ?recorder ;
+            sdmo:startAt ?startAt ;
+            sdmo:endAt ?endAt .
+        ?recorder
+            sdmo:localX ?x ;
+            sdmo:localY ?y ;
+            sdmo:localZ ?z .
+    }
+    `;
+};
+
 export const parseEvent = (dataArray) => {
   let events = [];
 
@@ -50,4 +80,49 @@ export const parseEvent = (dataArray) => {
   }
 
   return events;
+};
+
+export const parseViewer = (dataArray) => {
+  let ViewerData = {
+    duration: 0,
+    playlistFile: "",
+    audioFile: "",
+    spriteTimes: [],
+    positions: [],
+  };
+
+  for (let data of dataArray) {
+    const contentUrl = data.contentUrl.value;
+    const playerClassUri = data.playerClass.value;
+    const startAt = parseFloat(data.startAt.value);
+    const endAt = parseFloat(data.endAt.value);
+
+    // calc min(endAt - startAt)
+    if (ViewerData.duration === 0) {
+      ViewerData.duration = endAt - startAt;
+    } else {
+      ViewerData.duration = Math.min(ViewerData.duration, endAt - startAt);
+    }
+
+    if (playerClassUri.match(/VideoPlayer/)) {
+      ViewerData.playlistFile = contentUrl;
+    } else if (playerClassUri.match(/AudioPlayer/)) {
+      ViewerData.audioFile = contentUrl;
+
+      const eventTime = parseFloat(data.eventTime.value);
+      const x = parseFloat(data.x.value);
+      const y = parseFloat(data.y.value);
+      const z = parseFloat(data.z.value);
+
+      // set temporal value on `end`
+      ViewerData.spriteTimes.push({ start: eventTime, end: eventTime });
+      ViewerData.positions.push({ x: x, y: y, z: z });
+    }
+  }
+
+  for (let i = 0, len = ViewerData.spriteTimes.length; i < len; i++) {
+    ViewerData.spriteTimes[i].end += ViewerData.duration;
+  }
+
+  return ViewerData;
 };
