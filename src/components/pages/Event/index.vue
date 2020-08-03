37fdcff2fae16e68983a704353061f7f074eb2dc
shin <38515249+shin-elab@users.noreply.github.com>
2020-08-03 13:55:57 +0900

    feat: update viewer data loader

--- src/components/pages/Event/index.vue
+++ src/components/pages/Event/index.vue
@@ -17,21 +17,36 @@ export default {
       viewerData: parseViewer([]),
     };
   },
+  computed: {
+    eventId: function() {
+      return this.$route.query.id;
+    },
+  },
+  methods: {
+    sparqlFetch: function(eventId) {
+      sparqlAxios(
+        this.axios,
+        `${sparqlEndpointUrl}?query=${encodeURIComponent(
+          viewerQuery(eventId)
+        )}`,
+        (response) => {
+          const dataArray = response.data.results.bindings;
+          this.viewerData = parseViewer(dataArray);
+        },
+        (error) => {
+          console.error(error);
+          this.viewerData = parseViewer([]);
+        }
+      );
+    },
+  },
   created: function() {
-    const eventId = this.$route.query.id;
-
-    sparqlAxios(
-      this.axios,
-      `${sparqlEndpointUrl}?query=${encodeURIComponent(viewerQuery(eventId))}`,
-      (response) => {
-        const dataArray = response.data.results.bindings;
-        this.viewerData = parseViewer(dataArray);
-      },
-      (error) => {
-        console.error(error);
-        this.viewerData = parseViewer([]);
-      }
-    );
+    this.sparqlFetch(this.eventId);
+  },
+  watch: {
+    eventId: function(val) {
+      this.sparqlFetch(val);
+    },
   },
 };
 </script>
