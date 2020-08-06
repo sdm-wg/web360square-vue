6bba856274c2e4c879e0e3c751f40d916bd79e9b
shin <38515249+shin-elab@users.noreply.github.com>
2020-08-06 16:16:48 +0900

    feat: load audio using axios

--- src/components/pages/Event/index.vue
+++ src/components/pages/Event/index.vue
@@ -11,12 +11,17 @@ import {
   viewerQuery,
   parseViewer,
 } from "@/utils/sparql.js";
+import { audioAxios } from "@/utils/audio.js";
 
 export default {
   name: "Event",
   data: () => {
     return {
       viewerData: parseViewer([]),
+      webAudio: {
+        audioContext: null,
+        audioBuffer: null,
+      },
     };
   },
   computed: {
@@ -25,6 +30,10 @@ export default {
     },
   },
   methods: {
+    createAudioContext: function() {
+      const AudioContext = window.AudioContext || window.webkitAudioContext;
+      this.webAudio.audioContext = new AudioContext();
+    },
     sparqlFetch: function(eventId) {
       sparqlAxios(
         this.axios,
@@ -41,13 +50,36 @@ export default {
         }
       );
     },
+    loadAudio: function(audioFile) {
+      audioAxios(
+        this.axios,
+        audioFile,
+        (response) => {
+          this.webAudio.audioContext.decodeAudioData(
+            response.data,
+            (buffer) => {
+              this.webAudio.audioBuffer = buffer;
+            }
+          );
+        },
+        (error) => {
+          console.error(error);
+        }
+      );
+    },
   },
   created: function() {
+    this.createAudioContext();
     this.sparqlFetch(this.eventId);
   },
   watch: {
     eventId: function(val) {
       this.sparqlFetch(val);
+    },
+    "viewerData.audioFile": function(val) {
+      if (val.startsWith("http")) {
+        this.loadAudio(val);
+      }
     },
   },
   components: {
