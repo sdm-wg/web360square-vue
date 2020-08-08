9b7868bd7ba639ef7d3aed26dd7bdeb517c4217a
shin <38515249+shin-elab@users.noreply.github.com>
2020-08-08 15:36:27 +0900

    chore: remove gaze cursor

--- src/components/molecules/AFrameCamera/index.vue
+++ src/components/molecules/AFrameCamera/index.vue
@@ -1,14 +1,5 @@
 <template>
-  <a-entity camera look-controls position="0 1.6 0">
-    <a-entity
-      position="0 0 -1"
-      cursor="rayOrigin: mouse"
-      raycaster="objects: .clickable"
-      geometry="primitive: ring; radiusInner: 0.02; radiusOuter: 0.03"
-      material="color: black; shader: flat"
-    >
-    </a-entity>
-  </a-entity>
+  <a-entity camera look-controls position="0 1.6 0"> </a-entity>
 </template>
 
 <script>
