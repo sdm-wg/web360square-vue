a44906136b7662daf815a7201297aa9ac44cbc21
shin <38515249+shin-elab@users.noreply.github.com>
2020-08-08 15:52:48 +0900

    feat: apply listener to camera

--- src/components/molecules/AFrameCamera/index.vue
+++ src/components/molecules/AFrameCamera/index.vue
@@ -1,14 +1,32 @@
 <template>
-  <a-entity camera look-controls position="0 1.6 0"> </a-entity>
+  <a-entity listener camera look-controls position="0 1.6 0"> </a-entity>
 </template>
 
 <script>
+import { listener } from "@/utils/aframe/listener";
+
 const AudioContext = window.AudioContext || window.webkitAudioContext;
 
 export default {
   name: "AFrameCamera",
+  data: () => {
+    return {
+      listener: listener,
+    };
+  },
   props: {
     audioContext: AudioContext,
+  },
+  created: function() {
+    // HACK: re-reference `listener`
+    this.listener = listener;
+  },
+  watch: {
+    "listener.tickSignal": function() {
+      if (this.listener.initReady) {
+        this.listener.initReady = false;
+      }
+    },
   },
 };
 </script>
