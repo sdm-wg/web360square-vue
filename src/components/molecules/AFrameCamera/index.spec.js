e1f95af350cc858e961b17f994f4b1f44d97dc66
shin <38515249+shin-elab@users.noreply.github.com>
2020-08-08 15:22:12 +0900

    test: refactor test code

--- src/components/molecules/AFrameCamera/index.spec.js
+++ src/components/molecules/AFrameCamera/index.spec.js
@@ -2,9 +2,25 @@ import { shallowMount } from "@vue/test-utils";
 import AFrameCamera from ".";
 
 describe("molecules/AFrameCamera", () => {
+  let props;
+  let stubs;
+
+  beforeEach(() => {
+    props = { audioContext: null };
+    stubs = ["a-entity"];
+  });
+
+  it("checks props", () => {
+    const wrapper = shallowMount(AFrameCamera, {
+      propsData: props,
+      stubs: stubs,
+    });
+    expect(wrapper.props("audioContext")).toBe(props.audioContext);
+  });
+
   it("has an `a-entity`", () => {
     const wrapper = shallowMount(AFrameCamera, {
-      stubs: ["a-entity"],
+      stubs: stubs,
     });
     // Hack
     expect(wrapper.findAll("a-entity-stub").length).toBe(2);
