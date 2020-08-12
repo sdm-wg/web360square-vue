23adf5191647b6faa3866c5f3b0063f09adec2c4
shin <38515249+shin-elab@users.noreply.github.com>
2020-08-12 14:30:59 +0900

    feat: manage spectrum metadata on molecules/AFrameAudioVisualizer

--- src/components/molecules/AFrameAudioVisualizer/index.vue
+++ src/components/molecules/AFrameAudioVisualizer/index.vue
@@ -4,16 +4,33 @@
     geometry="primitive: sphere"
     :position="position"
     scale="0.3 0.3 0.3"
-    material="color: cyan; transparent: true; opacity: 0.9,"
+    material="color: gray; transparent: true; opacity: 0"
   >
   </a-entity>
 </template>
 
 <script>
+import { spectrumVectors } from "@/utils/aframe/audioVisualizer";
+
 export default {
   name: "AFrameAudioVisualizer",
+  data: () => {
+    return {
+      spectrums: [],
+    };
+  },
   props: {
     position: Object,
+  },
+  created: function() {
+    for (const [i, spectrumVector] of Object.entries(spectrumVectors)) {
+      this.spectrums[i] = {
+        vector: spectrumVector,
+        width: 0.15,
+        height: 0.1,
+        color: "gray",
+      };
+    }
   },
 };
 </script>
