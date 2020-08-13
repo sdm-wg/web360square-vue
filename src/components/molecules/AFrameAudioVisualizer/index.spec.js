5088743df45a70eeed8667fc4135522744a16203
shin <38515249+shin-elab@users.noreply.github.com>
2020-08-13 16:28:05 +0900

    test: add watcher test to molecules/AFrameAudioVisualizer

--- src/components/molecules/AFrameAudioVisualizer/index.spec.js
+++ src/components/molecules/AFrameAudioVisualizer/index.spec.js
@@ -2,11 +2,64 @@ import { shallowMount } from "@vue/test-utils";
 import AFrameAudioVisualizer from ".";
 
 describe("molecules/AFrameAudioVisualizer", () => {
+  // Props mock variables
+  let props;
+
+  // Stub variables
+  let stubs;
+
+  beforeEach(() => {
+    // Props mock
+    props = {
+      index: 0,
+      position: {},
+      webAudio: {
+        gains: [],
+      },
+    };
+
+    // Stubs
+    stubs = ["a-entity"];
+  });
+
+  it("checks registeredAudioVisualizer.num", async () => {
+    const wrapper = shallowMount(AFrameAudioVisualizer, {
+      propsData: props,
+      stubs: stubs,
+    });
+    wrapper.setData({
+      audioVisualizer: null,
+      registeredAudioVisualizer: { num: 1 },
+      spectrums: [],
+    });
+    await wrapper.vm.$nextTick();
+
+    // watch:registeredAudioVisualizer.num
+    expect(wrapper.vm.audioVisualizer).not.toBe(null);
+  });
+
+  it("checks audioVisualizer.tickSignal", async () => {
+    const wrapper = shallowMount(AFrameAudioVisualizer, {
+      propsData: props,
+      stubs: stubs,
+    });
+    wrapper.setData({
+      audioVisualizer: { tickSignal: true },
+      registeredAudioVisualizer: { num: 1 },
+      spectrums: [],
+    });
+    await wrapper.vm.$nextTick();
+
+    // watch:audioVisualizer.tickSignal
+    // (WIP)
+  });
+
   it("has an `a-entity`", () => {
     const wrapper = shallowMount(AFrameAudioVisualizer, {
-      stubs: ["a-entity"],
+      propsData: props,
+      stubs: stubs,
     });
-    // Hack
+    // HACK: find stub elements
     expect(wrapper.find("a-entity-stub").exists()).toBe(true);
   });
 });
