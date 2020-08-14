ebb1cd5956fb2d484077e47c6cb8011951d22aa4
shin <38515249+shin-elab@users.noreply.github.com>
2020-08-14 21:44:53 +0900

    test: change path of PlaySVG

--- src/components/atoms/PlaySVG/index.spec.js
+++ src/components/atoms/PlaySVG/index.spec.js
@@ -15,8 +15,8 @@ describe("atoms/PlaySVG", () => {
   it("svg is changed by props.isLoading and props.isPlaying", async () => {
     const pathD = {
       loading: "M12 3a9 9 0 1 0 9 9h-2a7 7 0 1 1-7-7v-2",
-      play: "M3 22v-20l18 10-18 10z",
-      pause: "M10 24h-6v-24h6v24zm10-24h-6v24h6v-24z",
+      play: "M7 18v-12l13 6-13 6z",
+      pause: "M10.5 7h-2v10h2v-10zM13.5 7h2v10h-2v-10z",
     };
     const wrapper = shallowMount(PlaySVG);
     wrapper.setProps({ isLoading: true, isPlay: false });
