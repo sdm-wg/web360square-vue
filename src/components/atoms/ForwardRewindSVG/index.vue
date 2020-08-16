82b8a8b5799b6eb69320eccae7fd3bcbf91dc855
shin <38515249+shin-elab@users.noreply.github.com>
2020-08-16 14:43:22 +0900

    feat: add atoms/ForwardRewindSVG component

---
+++ src/components/atoms/ForwardRewindSVG/index.vue
@@ -0,0 +1,36 @@
+<template>
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    class="fill-current"
+    :width="width"
+    viewBox="0 0 24 24"
+  >
+    <path
+      v-if="isForward"
+      d="M12 2.5a9.5 9.5 0 1 0 9.5 9.5h-1a8.5 8.5 0 1 1-8.5-8.5v-1M12 0v6l3-3-3-3zm3 0v6l3-3-3-3"
+    />
+    <path
+      v-else
+      d="M12 2.5a9.5 9.5 0 1 1 -9.5 9.5h1a8.5 8.5 0 1 0 8.5-8.5v-1M12 0v6l-3-3 3-3zm-3 0v6l-3-3 3-3"
+    />
+    <text
+      class="text-xs"
+      text-anchor="middle"
+      alignment-baseline="central"
+      x="12"
+      y="12"
+    >
+      10
+    </text>
+  </svg>
+</template>
+
+<script>
+export default {
+  name: "ForwardRewindSVG",
+  props: {
+    width: Number,
+    isForward: Boolean,
+  },
+};
+</script>
